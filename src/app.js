9eb69f8477e79df248af295faf6028b1cbd3e973
brendan.chang <brendanjchang@yahoo.com.hk>
2025-09-07 18:21:05 -0400

    added app.js and configs

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const {author, version } = require('../package.json');
+
+const logger = require('./logger');
+const pino = require('pino-http')({ logger });
+
+const app = express();
+
+app.use(pino);
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.setheader('Cache-Control', 'no-cache');
+  res.status(200).json({ status: 'ok', author, githuburl: 'https://github.com/Polymonomial/fragments.git', version });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ status: 'error', error: { code: 404, message: 'Not Found' } });
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Unable to process request';
+  if(status > 499) {
+    logger.error({err}, `Error processing request`);
+  }
+
+  res.status(status).json({
+    status: 'error',
+    error:{
+        message,
+        code: status,
+    }
+  })
+});
+
+module.exports = app;
+
+
+
