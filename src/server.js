2e0252942ef5c2a53e6bd396ea011479e9113bbf
brendan.chang <brendanjchang@yahoo.com.hk>
2025-09-08 04:14:29 -0400

    changed server.js to print all process environment variable

--- src/server.js
+++ src/server.js
@@ -3,4 +3,7 @@ const logger = require('./logger');
 const app = require('./app');
 const port = parseInt(process.env.PORT || '8080', 10);
 const server = stoppable(app.listen(port, () =>{logger.info(`Server started on port ${port}`)}));
+for (const key in process.env) {
+  logger.info(`${key}: ${process.env[key]}`);
+}
 module.exports = server;
