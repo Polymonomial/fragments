f460d8ae9f34cdf69fa51900174a0fa3b138c6c5
brendan.chang <brendanjchang@yahoo.com.hk>
2025-10-02 20:03:52 -0400

    testing unit test for memory.test.js

---
+++ tests/unit/memory.test.js
@@ -0,0 +1,13 @@
+const { writeFragment } = require('../../src/model/data/memory/index.js');
+//const MemoryDB = require('./memory-db.js');
+
+describe('memory db calls', () => {
+  // beforeEach(() => {
+  //   const data = new MemoryDB();
+  //   const metadata = new MemoryDB();
+  // });
+  test('writeFragment() returns nothing', async () => {
+    const result = writeFragment({ ownerid: 114514, id: '1919810', testdata: [] });
+    expect(result).toBe();
+  });
+});
